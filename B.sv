f262b034ffb739b19373a5f7193450fdddb99be4
msyksphinz-self <msyksphinz.dev@gmail.com>
2025-01-26 23:50:27 +0900

    Implement outD

--- B.sv
+++ B.sv
@@ -2,11 +2,13 @@ module B
   (
    input  inA,
    input  inB,
-   output outC
+   output outC,
+   output outD
    );
 
 always_comb begin
   outC = inA + inB;
+  outD = inA + inB;
 end
 
 endmodule // B
